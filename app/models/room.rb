5bc311ca74ee18c3aab0f9855bac6ee4ab1f9451
akiduki0526 <akiduki0526@gmail.com>
2022-11-25 15:51:22 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  has_many :room_users
+  #中間テーブルとroomsテーブルは多対多の関係であることを表している
+  has_many :users, through: :room_users
+  #usersテーブルとroomsテーブルは中間テーブルを介して(through)多対多の関係であることを表している
 end
